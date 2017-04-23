bc6227108dcf26459d03b337a36648f99629c6d6
Samssonart <samssonart@samssonarts-mbp.home>
2017-04-23 14:12:43 +0200

    Working ViewController transitions

---
+++ Implementation/iOSIlluminati/Illuminati/PanoramaViewController.h
@@ -0,0 +1,15 @@
+//
+//  PanoramaViewController.h
+//  Illuminati
+//
+//  Created by Samssonart on 4/23/17.
+//  Copyright © 2017 Samssonart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanoramaViewController : UIViewController
+
+-(IBAction)goToAR;
+
+@end
---
+++ Implementation/iOSIlluminati/Illuminati/PanoramaViewController.m
@@ -0,0 +1,46 @@
+//
+//  PanoramaViewController.m
+//  Illuminati
+//
+//  Created by Samssonart on 4/23/17.
+//  Copyright © 2017 Samssonart. All rights reserved.
+//
+
+#import "PanoramaViewController.h"
+
+@interface PanoramaViewController ()
+
+@end
+
+@implementation PanoramaViewController
+
+-(void)goToAR
+{
+    
+    UIStoryboard* ARStoryboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController* initialARVC = [ARStoryboard instantiateInitialViewController];
+    [self presentViewController:initialARVC animated:false completion:nil];
+    
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
